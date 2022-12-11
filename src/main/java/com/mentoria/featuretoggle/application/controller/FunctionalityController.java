97d1d5bf7178869fda8358eb049f0738689d887d
David lopes <marcosdavidsantos22@gmail.com>
2022-12-10 22:24:20 -0300

    creation of Functionality controller

---
+++ src/main/java/com/mentoria/featuretoggle/application/controller/FunctionalityController.java
@@ -0,0 +1,9 @@
+package com.mentoria.featuretoggle.application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/functionality")
+public class FunctionalityController {
+}
